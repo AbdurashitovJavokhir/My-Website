e64f5168a2f0997aeab3d5d2c7aa7bed1560d915
AbdurashitovJavokhir <javokhirabdurashitov@icloud.com>
2025-09-27 00:02:14 +0500

    Started to animate the full website

--- app.js
+++ app.js
@@ -21,3 +21,16 @@ let observer = new IntersectionObserver((entries) => {
 sections.forEach(section => {
   observer.observe(section);
 });
+
+document.getElementById("downloadBtn").addEventListener("click", function () {
+    // Path to your CV file
+    const cvFile = "CV.pdf";
+
+    // Create a temporary <a> element
+    const link = document.createElement("a");
+    link.href = cvFile;
+    link.download = "Javokhir-CV.pdf"; // File name for the download
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  });
